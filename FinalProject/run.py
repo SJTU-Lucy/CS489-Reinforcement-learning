71f4e2f724a80a81bf27aa58b889a75ca141e3b5
SJTU-Lucy <frenkiedejong@sjtu.edu.cn>
2022-05-15 10:22:51 +0800

    finishing dqn-pong

--- FinalProject/run.py
+++ FinalProject/run.py
@@ -1,16 +1,21 @@
-import os
 import argparse
-from test_pong import test_pong
+from DQN.test_pong import test_pong
+from DQN.train_pong import train_pong
 from PPO.test_ppo import test_ppo
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--env_name', type=str, default='PongNoFrameskip-v4', help='Name of the environment to run')
-parser.add_argument('--method', type=str, default='PPO', help='Method to use in MuJoCo environments, PPO or SAC')
+parser.add_argument('--env_name', type=str, default='PongNoFrameskip-v4', help='Name of the environment')
+parser.add_argument('--method', type=str, default='PPO', help='method for MuJoCo, PPO or DDPG')
+parser.add_argument('--action', type=str, default='test', help='run train or test')
 args = parser.parse_args()
 
 if args.env_name == 'PongNoFrameskip-v4':
     print('Environment: ' + args.env_name + ' Method: DQN')
-    test_pong()
+    if args.action == 'test':
+        test_pong()
+    else:
+        train_pong()
+
 elif args.env_name == 'Ant-v2':
     print('Environment: ' + args.env_name + ' Method: PPO')
-    test_ppo(args.env_name)
+    test_ppo(args.env_name, render=False)
